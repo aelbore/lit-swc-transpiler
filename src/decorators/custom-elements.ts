a4e866b817eaa342b58194b7c162d67dd9701d7f
aelbore <aelbroe.sg@gmail.com>
2021-08-12 21:51:55 +0800

    add script to format swc/core/Visitor.js to es format

--- src/decorators/custom-elements.ts
+++ src/decorators/custom-elements.ts
@@ -1,5 +1,5 @@
 import type { Module, Decorator, StringLiteral, Program, CallExpression } from '@swc/core'
-import Visitor from '@swc/core/Visitor.js'
+import Visitor from '@swc/visitor/Visitor.js'
 
 import * as swc from 'swc-ast-helpers'
 
